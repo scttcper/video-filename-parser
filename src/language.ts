56b937883cf030b42b613c5cb71066306ba9ca2e
Scott Cooper <scttcper@gmail.com>
2019-08-29 23:43:45 -0700

    setup language parser

--- src/language.ts
+++ src/language.ts
@@ -1,1 +1,189 @@
-export const languageRegex = /(?:\W|_|^)(?<italian>\b(?:ita|italian)\b)|(?<german>german\b|videomann)|(?<flemish>flemish)|(?<greek>greek)|(?<french>(?:\W|_)(?:FR|VOSTFR|VO|VFF|VFQ|VF2|TRUEFRENCH)(?:\W|_))|(?<russian>\brus\b)|(?<dutch>nl\W?subs?)|(?<hungarian>\b(?:HUNDUB|HUN)\b)|(?<hebrew>\bHebDub\b)|(?<czech>\b(?:CZ|SK)\b)|(?<ukrainian>\bukr\b)/i;
+import { parseTitleAndYear } from './title';
+
+/* eslint-disable complexity */
+export enum Language {
+  English = 'English',
+  French = 'French',
+  Spanish = 'Spanish',
+  German = 'German',
+  Italian = 'Italian',
+  Danish = 'Danish',
+  Dutch = 'Dutch',
+  Japanese = 'Japanese',
+  Cantonese = 'Cantonese',
+  Mandarin = 'Mandarin',
+  Russian = 'Russian',
+  Polish = 'Polish',
+  Vietnamese = 'Vietnamese',
+  Swedish = 'Swedish',
+  Norwegian = 'Norwegian',
+  Finnish = 'Finnish',
+  Turkish = 'Turkish',
+  Portuguese = 'Portuguese',
+  Flemish = 'Flemish',
+  Greek = 'Greek',
+  Korean = 'Korean',
+  Hungarian = 'Hungarian',
+  Hebrew = 'Hebrew',
+  Czech = 'Czech',
+  Ukrainian = 'Ukrainian',
+}
+
+export const languageExp = /(?:\W|_|^)(?<italian>\b(?:ita|italian)\b)|(?<german>german\b|videomann)|(?<flemish>flemish)|(?<greek>greek)|(?<french>(?:\W|_)(?:FR|VOSTFR|VO|VFF|VFQ|VF2|TRUEFRENCH)(?:\W|_))|(?<russian>\brus\b)|(?<dutch>nl\W?subs?)|(?<hungarian>\b(?:HUNDUB|HUN)\b)|(?<hebrew>\bHebDub\b)|(?<czech>\b(?:CZ|SK)\b)|(?<ukrainian>\bukr\b)/i;
+
+export function parseLanguage(title: string): Language[] {
+  const parsedTitle = parseTitleAndYear(title, true).title;
+  const languageTitle = title.replace(parsedTitle, '').toLowerCase();
+  const languages: Language[] = [];
+
+  for (const language of Object.keys(Language)) {
+    if (languageTitle.includes(language.toLowerCase())) {
+      languages.push(Language[language]);
+    }
+  }
+
+  if (languageTitle.includes('english')) {
+    languages.push(Language.English);
+  }
+
+  if (languageTitle.includes('french')) {
+    languages.push(Language.French);
+  }
+
+  if (languageTitle.includes('spanish')) {
+    languages.push(Language.Spanish);
+  }
+
+  if (languageTitle.includes('danish')) {
+    languages.push(Language.Danish);
+  }
+
+  if (languageTitle.includes('dutch')) {
+    languages.push(Language.Dutch);
+  }
+
+  if (languageTitle.includes('japanese')) {
+    languages.push(Language.Japanese);
+  }
+
+  if (languageTitle.includes('cantonese')) {
+    languages.push(Language.Cantonese);
+  }
+
+  if (languageTitle.includes('mandarin')) {
+    languages.push(Language.Mandarin);
+  }
+
+  if (languageTitle.includes('korean')) {
+    languages.push(Language.Korean);
+  }
+
+  if (languageTitle.includes('russian')) {
+    languages.push(Language.Russian);
+  }
+
+  if (languageTitle.includes('polish')) {
+    languages.push(Language.Polish);
+  }
+
+  if (languageTitle.includes('vietnamese')) {
+    languages.push(Language.Vietnamese);
+  }
+
+  if (languageTitle.includes('swedish')) {
+    languages.push(Language.Swedish);
+  }
+
+  if (languageTitle.includes('norwegian')) {
+    languages.push(Language.Norwegian);
+  }
+
+  if (languageTitle.includes('nordic')) {
+    languages.push(Language.Norwegian);
+  }
+
+  if (languageTitle.includes('finnish')) {
+    languages.push(Language.Finnish);
+  }
+
+  if (languageTitle.includes('turkish')) {
+    languages.push(Language.Turkish);
+  }
+
+  if (languageTitle.includes('portuguese')) {
+    languages.push(Language.Portuguese);
+  }
+
+  if (languageTitle.includes('hungarian')) {
+    languages.push(Language.Hungarian);
+  }
+
+  if (languageTitle.includes('hebrew')) {
+    languages.push(Language.Hebrew);
+  }
+
+  if (languageTitle.includes('czech')) {
+    languages.push(Language.Czech);
+  }
+
+  if (languageTitle.includes('ukrainian')) {
+    languages.push(Language.Ukrainian);
+  }
+
+  const result = languageExp.exec(languageTitle);
+  if (result && result.groups) {
+    if (result.groups.italian) {
+      languages.push(Language.Italian);
+    }
+
+    if (result.groups.german) {
+      languages.push(Language.German);
+    }
+
+    if (result.groups.flemish) {
+      languages.push(Language.Flemish);
+    }
+
+    if (result.groups.greek) {
+      languages.push(Language.Greek);
+    }
+
+    if (result.groups.french) {
+      languages.push(Language.French);
+    }
+
+    if (result.groups.russian) {
+      languages.push(Language.Russian);
+    }
+
+    if (result.groups.dutch) {
+      languages.push(Language.Dutch);
+    }
+
+    if (result.groups.hungarian) {
+      languages.push(Language.Hungarian);
+    }
+
+    if (result.groups.hebrew) {
+      languages.push(Language.Hebrew);
+    }
+
+    if (result.groups.czech) {
+      languages.push(Language.Czech);
+    }
+
+    if (result.groups.ukrainian) {
+      languages.push(Language.Ukrainian);
+    }
+  }
+
+  if (languageTitle.includes('multi')) {
+    languages.push(Language.English);
+  }
+
+  if (languages.length === 0) {
+    languages.push(Language.English);
+  }
+
+  return [...new Set(languages)];
+}
---
+++ test/language.spec.ts
@@ -0,0 +1,54 @@
+import { parseLanguage, Language } from '../src';
+
+describe('', () => {
+  const tests: Array<[string, Language[]]> = [
+    ['Castle.2009.S01E14.English.HDTV.XviD-LOL', [Language.English]],
+    ['Castle.2009.S01E14.French.HDTV.XviD-LOL', [Language.French]],
+    ['Ouija.Origin.of.Evil.2016.MULTi.TRUEFRENCH.1080p.BluRay.x264-MELBA', [Language.French, Language.English]],
+    ['Everest.2015.FRENCH.VFQ.BDRiP.x264-CNF30', [Language.French]],
+    [
+      'Showdown.In.Little.Tokyo.1991.MULTI.VFQ.VFF.DTSHD-MASTER.1080p.BluRay.x264-ZombiE',
+      [Language.French, Language.English],
+    ],
+    ['The.Polar.Express.2004.MULTI.VF2.1080p.BluRay.x264-PopHD', [Language.French, Language.English]],
+    ['Castle.2009.S01E14.Spanish.HDTV.XviD-LOL', [Language.Spanish]],
+    ['Castle.2009.S01E14.German.HDTV.XviD-LOL', [Language.German]],
+    ['Castle.2009.S01E14.Italian.HDTV.XviD-LOL', [Language.Italian]],
+    ['Castle.2009.S01E14.Danish.HDTV.XviD-LOL', [Language.Danish]],
+    ['Castle.2009.S01E14.Dutch.HDTV.XviD-LOL', [Language.Dutch]],
+    ['Castle.2009.S01E14.Japanese.HDTV.XviD-LOL', [Language.Japanese]],
+    ['Castle.2009.S01E14.Cantonese.HDTV.XviD-LOL', [Language.Cantonese]],
+    ['Castle.2009.S01E14.Mandarin.HDTV.XviD-LOL', [Language.Mandarin]],
+    ['Castle.2009.S01E14.Korean.HDTV.XviD-LOL', [Language.Korean]],
+    ['Castle.2009.S01E14.Russian.HDTV.XviD-LOL', [Language.Russian]],
+    ['Castle.2009.S01E14.Ukrainian.HDTV.XviD-LOL', [Language.Ukrainian]],
+    ['Castle.2009.S01E14.Ukr.HDTV.XviD-LOL', [Language.Ukrainian]],
+    ['Castle.2009.S01E14.Polish.HDTV.XviD-LOL', [Language.Polish]],
+    ['Castle.2009.S01E14.Vietnamese.HDTV.XviD-LOL', [Language.Vietnamese]],
+    ['Castle.2009.S01E14.Swedish.HDTV.XviD-LOL', [Language.Swedish]],
+    ['Castle.2009.S01E14.Norwegian.HDTV.XviD-LOL', [Language.Norwegian]],
+    ['Castle.2009.S01E14.Finnish.HDTV.XviD-LOL', [Language.Finnish]],
+    ['Castle.2009.S01E14.Turkish.HDTV.XviD-LOL', [Language.Turkish]],
+    ['Castle.2009.S01E14.Czech.HDTV.XviD-LOL', [Language.Czech]],
+    ['Castle.2009.S01E14.Portuguese.HDTV.XviD-LOL', [Language.Portuguese]],
+    ['Burn.Notice.S04E15.Brotherly.Love.GERMAN.DUBBED.WS.WEBRiP.XviD.REPACK-TVP', [Language.German]],
+    ['Revolution S01E03 No Quarter 2012 WEB-DL 720p Nordic-philipo mkv', [Language.Norwegian]],
+    ['Constantine.2014.S01E01.WEBRiP.H264.AAC.5.1-NL.SUBS', [Language.Dutch]],
+    ['Castle.2009.S01E14.HDTV.XviD.HUNDUB-LOL', [Language.Hungarian]],
+    ['Castle.2009.S01E14.HDTV.XviD.ENG.HUN-LOL', [Language.Hungarian]],
+    ['Castle.2009.S01E14.HDTV.XviD.HUN-LOL', [Language.Hungarian]],
+    ['Castle.2009.S01E14.HDTV.XviD.CZ-LOL', [Language.Czech]],
+    ['Passengers.2016.German.DL.AC3.Dubbed.1080p.WebHD.h264.iNTERNAL-PsO', [Language.German]],
+    ['Der.Soldat.James.German.Bluray.FuckYou.Pso.Why.cant.you.follow.scene.rules.1998', [Language.German]],
+    ['Passengers.German.DL.AC3.Dubbed..BluRay.x264-PsO', [Language.German]],
+    ['Valana la Legende FRENCH BluRay 720p 2016 kjhlj', [Language.French]],
+    ['Smurfs.​The.​Lost.​Village.​2017.​1080p.​BluRay.​HebDub.​x264-​iSrael', [Language.Hebrew]],
+    ['The Danish Girl 2015', [Language.English]],
+    ['Nocturnal Animals (2016) MULTi VFQ English [1080p] BluRay x264-PopHD', [Language.English, Language.French]],
+    ['Wonder.Woman.2017.720p.BluRay.DD5.1.x264-TayTO.CZ-FTU', [Language.Czech]],
+    ['Fantastic.Beasts.The.Crimes.Of.Grindelwald.2018.2160p.WEBRip.x265.10bit.HDR.DD5.1-GASMASK', [Language.English]],
+  ];
+  test.each(tests)('should get language of "%s"', (title, languages) => {
+    expect(parseLanguage(title)).toEqual(languages);
+  });
+});
