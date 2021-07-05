caf20bda8bce405ab8a62bfd26ee0972b08ec798
Scott Cooper <scttcper@gmail.com>
2021-07-05 15:41:16 -0700

    feat: Add WMV, split x264 and h264

--- test/codec.spec.ts
+++ test/codec.spec.ts
@@ -12,14 +12,16 @@ describe('parseVideoCodec', () => {
     ['Vice 2018 DVDScr Xvid AC3 HQ Hive-CM8', VideoCodec.XVID],
     ['The Dark Knight[2008]DvDrip-aXXo [pendhu]', undefined],
     ['Bridesmaids[2011][Unrated Edition]DvDrip AC3-aXXo', undefined],
-    ['Get Out 2017 BluRay 10Bit 1080p DD5 1 H265-d3g', VideoCodec.X265],
+    ['Get Out 2017 BluRay 10Bit 1080p DD5 1 H265-d3g', VideoCodec.H265],
     ['Minions 2015 720p HC HDRip X265 AC3 TiTAN', VideoCodec.X265],
-    ["Marvel's The Avengers 2012 BluRay 1080p DD5 1 10Bit H265-d3g", VideoCodec.X265],
+    ["Marvel's The Avengers 2012 BluRay 1080p DD5 1 10Bit H265-d3g", VideoCodec.H265],
     ['Exodus Gods and Kings 2014 MULTi 2160p UHD BluRay x265-SESKAPiLE', VideoCodec.X265],
     ['The Incredibles 2004 BluRay x264-jlw', VideoCodec.X264],
     ['Jack Reacher 2012 720p BluRay X264-AMIABLE', VideoCodec.X264],
-    ['Super Troopers 2 2018 1080p WEB-DL H264 AC3-EVO', VideoCodec.X264],
+    ['Super Troopers 2 2018 1080p WEB-DL H264 AC3-EVO', VideoCodec.H264],
     ['The.Middle.720p.HEVC-MeGusta-Pre', VideoCodec.X265],
+    ['Cloud.Atlas.2012.BluRay.1080p.VC1.5.1.WMV-INSECTS', VideoCodec.WMV],
+    ['The.Book.Of.Eli.2010.Bluray.VC1.1080P.5.1.WMV-NOVO', VideoCodec.WMV],
   ];
   test.each(cases)('should parse codec for "%s"', (title, expected) => {
     expect(parseVideoCodec(title).codec).toBe(expected);
