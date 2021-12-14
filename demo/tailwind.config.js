1f0b0b925588a70077e042bfc92df7bbbb977539
Scott Cooper <scttcper@gmail.com>
2021-12-14 08:53:58 -0800

    chore: upgrade dep

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -2,12 +2,12 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media',
   theme: {
     extend: {
       colors: {
-        gray: colors.blueGray,
+        gray: colors.slate,
       },
     },
   },
