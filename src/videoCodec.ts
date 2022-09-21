85a117a82ebaa16d5e1d602b67c836ad2a5ceaba
Scott Cooper <scttcper@gmail.com>
2022-09-20 23:00:40 -0700

    fix lint

--- src/videoCodec.ts
+++ src/videoCodec.ts
@@ -51,7 +51,7 @@ export function parseVideoCodec(title: string): { codec?: VideoCodec; source?: s
     return { codec: VideoCodec.H265, source: groups['h265'] };
   }
 
-  if (groups['x265'] || groups['hevc']) {
+  if (groups['x265'] ?? groups['hevc']) {
     return { codec: VideoCodec.X265, source: groups['x265'] ?? groups['hevc'] };
   }
 
@@ -59,7 +59,7 @@ export function parseVideoCodec(title: string): { codec?: VideoCodec; source?: s
     return { codec: VideoCodec.X264, source: groups['x264'] };
   }
 
-  if (groups['xvidhd'] || groups['xvid'] || groups['divx']) {
+  if (groups['xvidhd'] ?? groups['xvid'] ?? groups['divx']) {
     return { codec: VideoCodec.XVID, source: groups['xvidhd'] ?? groups['xvid'] ?? groups['divx'] };
   }
 
