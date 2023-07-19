af6031f2770006905847feaeaf2aa2ececbdabbb
Scott Cooper <scttcper@gmail.com>
2023-07-18 21:08:09 -0700

    test: enable threads

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    singleThread: true,
     coverage: {
       provider: 'c8',
       reporter: ['text', 'json', 'html'],
