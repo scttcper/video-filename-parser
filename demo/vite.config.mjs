7b22d57c77d07eff626d8b966ec7e3516349b6ce
Scott Cooper <scttcper@gmail.com>
2025-04-16 20:40:15 -0700

    chore: add more demo packages

--- demo/vite.config.mjs
+++ demo/vite.config.mjs
@@ -1,8 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: '../build',
   },
