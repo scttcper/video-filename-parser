f8aacadc15aac06676078a7347c10a25b28a83f9
Scott Cooper <scttcper@gmail.com>
2025-10-29 21:47:26 -0700

    chore: upgrade dep

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    exclude: ['node_modules', 'dist', '.git'],
     coverage: {
       include: ['src/**'],
       reporter: ['text', 'json', 'html'],
