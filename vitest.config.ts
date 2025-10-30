af5a51055cde09520026fd5f0ee27d7f4829982a
Scott Cooper <scttcper@gmail.com>
2025-10-29 21:48:42 -0700

    test: ignore more

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['node_modules', 'dist', '.git'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.git/**'],
     coverage: {
       include: ['src/**'],
       reporter: ['text', 'json', 'html'],
