6b9b770e1205cf723cdaf90530ae6fc8fdfe15eb
Scott Cooper <scttcper@gmail.com>
2026-05-30 07:52:01 -0700

    ref(audio): Use codec pattern table

--- src/audioCodec.ts
+++ src/audioCodec.ts
@@ -1,38 +1,3 @@
-const mp3CodecExp = /\b(?<mp3>(LAME(?:\d)+-?(?:\d)+)|(mp3))\b/i;
-const mp2CodecExp = /\b(?<mp2>(mp2))\b/i;
-const dolbyCodecExp = /\b(?<dolby>(Dolby)|(Dolby-?Digital)|(DD)|(AC3D?))\b/i;
-const dolbyAtmosCodecExp = /\b(?<dolbyatmos>(Dolby-?Atmos))\b/i;
-const aacAtmosCodecExp = /\b(?<aac>(AAC))(\d?.?\d?)(ch)?\b/i;
-const eac3CodecExp = /\b(?<eac3>(EAC3|DDP|DD\+))\b/i;
-const flacCodecExp = /\b(?<flac>(FLAC))\b/i;
-const dtsCodecExp = /\b(?<dts>(DTS))\b/i;
-const dtsHdCodecExp = /\b(?<dtshd>(DTS-?HD)|(DTS(?=-?MA)|(DTS-X)))\b/i;
-const trueHdCodecExp = /\b(?<truehd>(True-?HD))\b/i;
-const opusCodecExp = /\b(?<opus>(Opus))\b/i;
-const vorbisCodecExp = /\b(?<vorbis>(Vorbis))\b/i;
-const pcmCodecExp = /\b(?<pcm>(PCM))\b/i;
-const lpcmCodecExp = /\b(?<lpcm>(LPCM))\b/i;
-
-const audioCodecExp = new RegExp(
-  [
-    mp3CodecExp.source,
-    mp2CodecExp.source,
-    dolbyCodecExp.source,
-    dolbyAtmosCodecExp.source,
-    aacAtmosCodecExp.source,
-    eac3CodecExp.source,
-    flacCodecExp.source,
-    dtsHdCodecExp.source,
-    dtsCodecExp.source,
-    trueHdCodecExp.source,
-    opusCodecExp.source,
-    vorbisCodecExp.source,
-    pcmCodecExp.source,
-    lpcmCodecExp.source,
-  ].join('|'),
-  'i',
-);
-
 export enum AudioCodec {
   MP3 = 'MP3',
   MP2 = 'MP2',
@@ -49,68 +14,35 @@ export enum AudioCodec {
   LPCM = 'LPCM',
 }
 
+interface AudioCodecPattern {
+  name: string;
+  codec: AudioCodec;
+  regex: RegExp;
+}
+
+const audioCodecPatterns: AudioCodecPattern[] = [
+  { name: 'dolby-atmos', codec: AudioCodec.EAC3, regex: /\bDolby[-_. ]?Atmos\b/i },
+  { name: 'eac3', codec: AudioCodec.EAC3, regex: /\b(?:EAC3|DDP|DD\+)\b/i },
+  { name: 'truehd', codec: AudioCodec.TRUEHD, regex: /\bTrue-?HD\b/i },
+  { name: 'dolby', codec: AudioCodec.DOLBY, regex: /\b(?:Dolby-?Digital|Dolby|DD|AC3D?)\b/i },
+  { name: 'dts-hd', codec: AudioCodec.DTSHD, regex: /\b(?:DTS-?HD|DTS-?MA|DTS-X)\b/i },
+  { name: 'dts', codec: AudioCodec.DTS, regex: /\bDTS\b/i },
+  { name: 'aac', codec: AudioCodec.AAC, regex: /\bAAC(?=(?:\d(?:[.\s]?\d)?)?(?:ch)?\b)/i },
+  { name: 'flac', codec: AudioCodec.FLAC, regex: /\bFLAC\b/i },
+  { name: 'mp3', codec: AudioCodec.MP3, regex: /\b(?:LAME(?:\d)+-?(?:\d)+|mp3)\b/i },
+  { name: 'mp2', codec: AudioCodec.MP2, regex: /\bmp2\b/i },
+  { name: 'lpcm', codec: AudioCodec.LPCM, regex: /\bLPCM\b/i },
+  { name: 'pcm', codec: AudioCodec.PCM, regex: /\bPCM\b/i },
+  { name: 'opus', codec: AudioCodec.OPUS, regex: /\bOpus\b/i },
+  { name: 'vorbis', codec: AudioCodec.VORBIS, regex: /\bVorbis\b/i },
+];
+
 export function parseAudioCodec(title: string): { codec?: AudioCodec; source?: string } {
-  const audioResult = audioCodecExp.exec(title);
-  if (!audioResult?.groups) {
-    return {};
-  }
-
-  const { groups } = audioResult;
-
-  if (groups.aac) {
-    return { codec: AudioCodec.AAC, source: groups.aac };
-  }
-
-  if (groups.dolbyatmos) {
-    return { codec: AudioCodec.EAC3, source: groups.dolbyatmos };
-  }
-
-  if (groups.dolby) {
-    return { codec: AudioCodec.DOLBY, source: groups.dolby };
-  }
-
-  if (groups.dtshd) {
-    return { codec: AudioCodec.DTSHD, source: groups.dtshd };
-  }
-
-  if (groups.dts) {
-    return { codec: AudioCodec.DTS, source: groups.dts };
-  }
-
-  if (groups.flac) {
-    return { codec: AudioCodec.FLAC, source: groups.flac };
-  }
-
-  if (groups.truehd) {
-    return { codec: AudioCodec.TRUEHD, source: groups.truehd };
-  }
-
-  if (groups.mp3) {
-    return { codec: AudioCodec.MP3, source: groups.mp3 };
-  }
-
-  if (groups.mp2) {
-    return { codec: AudioCodec.MP2, source: groups.mp2 };
-  }
-
-  if (groups.pcm) {
-    return { codec: AudioCodec.PCM, source: groups.pcm };
-  }
-
-  if (groups.lpcm) {
-    return { codec: AudioCodec.LPCM, source: groups.lpcm };
-  }
-
-  if (groups.opus) {
-    return { codec: AudioCodec.OPUS, source: groups.opus };
-  }
-
-  if (groups.vorbis) {
-    return { codec: AudioCodec.VORBIS, source: groups.vorbis };
-  }
-
-  if (groups.eac3) {
-    return { codec: AudioCodec.EAC3, source: groups.eac3 };
+  for (const { codec, regex } of audioCodecPatterns) {
+    const result = regex.exec(title);
+    if (result) {
+      return { codec, source: result[0] };
+    }
   }
 
   return {};
--- test/audioCodec.spec.ts
+++ test/audioCodec.spec.ts
@@ -5,7 +5,7 @@ import { AudioCodec, parseAudioCodec } from '../src/audioCodec.js';
 const audioCodecCases: Array<[string, ReturnType<typeof parseAudioCodec>]> = [
   [
     'Hannibal 2001 4K UHD Dolby Vision MP4 DD+5 1 H265-d3g',
-    { codec: AudioCodec.DOLBY, source: 'Dolby' },
+    { codec: AudioCodec.EAC3, source: 'DD+' },
   ],
   ['Aladdin 2019 720p BluRay x264 AC3 5 1-OMEGA', { codec: AudioCodec.DOLBY, source: 'AC3' }],
   ['Trespass Against Us (2017) 1080p BluRay x265 6ch -Dtech mkv', {}],
@@ -38,6 +38,21 @@ const audioCodecCases: Array<[string, ReturnType<typeof parseAudioCodec>]> = [
     'Frozen.2.2019.German.DL.EAC3.1080p.DSNP.WEB.H265-ZeroTwo',
     { codec: AudioCodec.EAC3, source: 'EAC3' },
   ],
+  [
+    'Movie.Title.2024.1080p.WEB-DL.Dolby.Atmos.H265-GROUP',
+    { codec: AudioCodec.EAC3, source: 'Dolby.Atmos' },
+  ],
+  [
+    'Movie.Title.2024.1080p.BluRay.DTS-MA.x264-GROUP',
+    { codec: AudioCodec.DTSHD, source: 'DTS-MA' },
+  ],
+  ['Movie.Title.2024.1080p.BluRay.LPCM.x264-GROUP', { codec: AudioCodec.LPCM, source: 'LPCM' }],
+  ['Movie.Title.2024.1080p.BluRay.PCM.x264-GROUP', { codec: AudioCodec.PCM, source: 'PCM' }],
+  ['Movie.Title.2024.1080p.BluRay.FLAC.x264-GROUP', { codec: AudioCodec.FLAC, source: 'FLAC' }],
+  ['Movie.Title.2024.1080p.WEB.Opus.x265-GROUP', { codec: AudioCodec.OPUS, source: 'Opus' }],
+  ['Movie.Title.2024.1080p.WEB.Vorbis.x265-GROUP', { codec: AudioCodec.VORBIS, source: 'Vorbis' }],
+  ['Movie.Title.2024.DVDRip.LAME3-99.XviD-GROUP', { codec: AudioCodec.MP3, source: 'LAME3-99' }],
+  ['Movie.Title.2024.DVDRip.MP2.XviD-GROUP', { codec: AudioCodec.MP2, source: 'MP2' }],
 ];
 
 for (const [title, result] of audioCodecCases) {
